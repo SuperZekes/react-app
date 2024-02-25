227cd6e0b45474154473e776f82464259e087d6e
SuperZekes <markandsheenaknowles@gmail.com>
2024-02-25 05:13:24 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://superzekes.com" target="_blank">
+          <img src="https://superzekes.com/images/superzekes.png" className="logo" alt="SuperZekes Icon" />
+        </a>
+
+      </div>
+      <h1>Welcome to This Example Site</h1>
+      
+       <p className="read-the-docs">
+        Click on my profile picture to learn more about me
+      </p>
+    </div>
+  )
+}
+
+export default App
