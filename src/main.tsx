797eaae833446be57951f9cc82b3b615aacc1456
SuperZekes <markandsheenaknowles@gmail.com>
2024-02-25 05:20:20 -0400

    Take care of useless code

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://superzekes.com" target="_blank">
-          <img src="https://superzekes.com/images/superzekes.png" className="logo" alt="SuperZekes Icon" />
-        </a>
-
-      </div>
-      <h1>Welcome to This Example Site</h1>
-      
-       <p className="read-the-docs">
-        Click on my profile picture to learn more about me
-      </p>
-    </div>
-  )
-}
-
-export default App
-
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
